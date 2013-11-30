965d333a4a18303acb062662c558d31f450f4bd3
Matthew Molloy <whamtet@gmail.com>
2013-11-30 19:22:35 +1000

    w

--- src/cljs/excellent/io.cljs
+++ src/cljs/excellent/io.cljs
@@ -1,10 +1,14 @@
 (ns excellent.io
   (:require [himera.client.repl :as repl]))
 
-(defn ^:export slurp [i]
+(defn ^:export slurp
+  "Slurp contents from tab i."
+  [i]
   (->> i (str "#workspace") js/jQuery .val))
 
-(defn ^:export spit [i val]
+(defn ^:export spit
+  "Spit val into tab i."
+  [i val]
   (if (@tabs i)
     (.val (js/jQuery (str "#workspace" i)) val)
     (add-tab val)))
@@ -15,13 +19,19 @@
 (defn double-map [f grid]
   (map #(map f %) grid))
 
-(defn ^:export grid-slurp [i]
+(defn ^:export grid-slurp
+  "Slurp tab i into a two-dimensional grid."
+  [i]
   (map split-line (.split (slurp i) "\n")))
 
-(defn ^:export grid-slurp-num [i]
-  (double-map #(js/Number %) (grid-slurp i)))
+(defn ^:export grid-slurp-num
+  "Slurp tab i into a two-dimensional grid of numbers."
+  [i]
+  (double-map js/Number (grid-slurp i)))
 
-(defn ^:export excel [i]
+(defn ^:export excel
+  "Download excel spreadsheet with contents from tab i."
+  [i]
   (.val (js/jQuery "#exceltext") (slurp i))
   (.submit (js/jQuery "#excelform")))
 
@@ -42,7 +52,9 @@
 (defn ^:export open-tab [i]
   (swap! tabs #(conj % i)))
 
-(defn ^:export save []
+(defn ^:export save
+  "Download contents of tabs to preserve your work."
+  []
   (.val (js/jQuery "#savetext") (pr-str (save-map)))
   (.submit (js/jQuery "#saveform")))
 
@@ -53,8 +65,12 @@
   (.click (js/jQuery "#addtab"))
   (set-latest-tab val))
 
-(defn ^:export load [i]
+(defn ^:export load
+  "Load contents of tab i as clojurescript."
+  [i]
   (js/eval (repl/simple-compile (slurp i))))
 
-(defn ^:export js-load [i]
+(defn ^:export js-load
+  "Load contents of tab i as javascript."
+  [i]
   (js/eval (slurp i)))
